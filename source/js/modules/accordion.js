216afa781c69dd3fca5c5e87bddb8a5ec25cb366
Valera Komissarenko <nuberman88@gmail.com>
2023-02-28 02:18:23 +0400

    fixed js of accordion

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,18 +1,26 @@
 const buttons = document.querySelectorAll('.main-footer__accordion-toggler');
 const accordions = document.querySelectorAll('.main-footer__accordion');
+const headers = document.querySelectorAll('.main-footer__section-header');
 
 const moveAccordion = () => {
   buttons.forEach((button, index) => {
 
     button.addEventListener('click', () => {
       if (accordions[index].classList.contains('main-footer__accordion--hidden')) {
-        accordions.forEach((content, current) =>
-          index !== current ?
-            content.classList.add('main-footer__accordion--hidden') :
-            accordions[index].classList.remove('main-footer__accordion--hidden')
+        accordions.forEach((content, current) => {
+          if (index !== current) {
+            content.classList.add('main-footer__accordion--hidden');
+            headers[current].classList.remove('main-footer__section-header--open');
+          } else {
+            accordions[index].classList.remove('main-footer__accordion--hidden');
+            headers[index].classList.add('main-footer__section-header--open');
+          }
+        }
+
         );
       } else {
         accordions[index].classList.add('main-footer__accordion--hidden');
+        headers[index].classList.remove('main-footer__section-header--open');
       }
     });
   });
