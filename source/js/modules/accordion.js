b6aeab3e94f8e8e65d6ff06149905fb7afecb345
Valera Komissarenko <nuberman88@gmail.com>
2023-02-28 01:51:01 +0400

    programmed accordion buttons

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -6,7 +6,11 @@ const moveAccordion = () => {
 
     button.addEventListener('click', () => {
       if (accordions[index].classList.contains('main-footer__accordion--hidden')) {
-        accordions[index].classList.remove('main-footer__accordion--hidden');
+        accordions.forEach((content, current) =>
+          index !== current ?
+            content.classList.add('main-footer__accordion--hidden') :
+            accordions[index].classList.remove('main-footer__accordion--hidden')
+        );
       } else {
         accordions[index].classList.add('main-footer__accordion--hidden');
       }
