8b05d954b5dcf50fb375b1cf92fb9b299165fb62
Valera Komissarenko <nuberman88@gmail.com>
2023-02-28 01:38:33 +0400

    styled footer and moved accordion. draft version

---
+++ source/js/modules/accordion.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('.main-footer__accordion-toggler');
+const accordions = document.querySelectorAll('.main-footer__accordion');
+
+const moveAccordion = () => {
+  buttons.forEach((button, index) => {
+
+    button.addEventListener('click', () => {
+      if (accordions[index].classList.contains('main-footer__accordion--hidden')) {
+        accordions[index].classList.remove('main-footer__accordion--hidden');
+      } else {
+        accordions[index].classList.add('main-footer__accordion--hidden');
+      }
+    });
+  });
+};
+
+moveAccordion();
