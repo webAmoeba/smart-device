b9dc889d35d2837f2229e3fd01bee713a95d0f9e
Valera Komissarenko <nuberman88@gmail.com>
2023-03-07 05:54:13 +0400

    added mask for phone-input

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,13 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+
+import {initPhoneMask} from './vendor/phone-mask';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initPhoneMask();
+  });
+});
+
