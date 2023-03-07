eebbeb2f581c1deecab97387cd1ac673124decf1
Valera Komissarenko <nuberman88@gmail.com>
2023-03-07 10:03:53 +0400

    fixed a little

--- source/js/modules/show-more.js
+++ source/js/modules/show-more.js
@@ -1,23 +1,43 @@
 const button = document.querySelector('.about-company__button');
-const descriptions = document.querySelectorAll('.about-company p');
+const descriptions = document.querySelectorAll('.about-company__grid p');
 const mobileDescriptions = document.querySelectorAll('.about-company__mobile-description');
 
 button.style.display = 'flex';
-descriptions.forEach((elem, index) => index > 1 ? elem.classList.add('about-company__description--hidden') : '');
-mobileDescriptions.forEach((elem) => elem.classList.add('about-company__mobile-description--hidden'));
+
+if (descriptions) {
+  descriptions.forEach((elem, index) => index > 1 ? elem.classList.add('about-company__description--hidden') : '');
+}
+
+if (mobileDescriptions) {
+  mobileDescriptions.forEach((elem) => elem.classList.add('about-company__mobile-description--hidden'));
+}
 
 const showMore = () => {
   button.addEventListener('click', () => {
     if (button.classList.contains('about-company__button--rollup')) {
-      descriptions.forEach((elem, index) => index > 1 ? elem.classList.add('about-company__description--hidden') : '');
-      mobileDescriptions.forEach((elem) => elem.classList.add('about-company__mobile-description--hidden'));
-      button.classList.remove('about-company__button--rollup');
-      button.textContent = 'подробнее';
+      if (descriptions) {
+        descriptions.forEach((elem, index) => index > 1 ? elem.classList.add('about-company__description--hidden') : '');
+      }
+      if (mobileDescriptions) {
+        mobileDescriptions.forEach((elem) => elem.classList.add('about-company__mobile-description--hidden'));
+      }
+      if (button) {
+        button.classList.remove('about-company__button--rollup');
+        button.textContent = 'подробнее';
+      }
+
     } else {
-      descriptions.forEach((elem) => elem.classList.remove('about-company__description--hidden'));
-      mobileDescriptions.forEach((elem) => elem.classList.remove('about-company__mobile-description--hidden'));
-      button.classList.add('about-company__button--rollup');
-      button.textContent = 'свернуть';
+
+      if (descriptions) {
+        descriptions.forEach((elem) => elem.classList.remove('about-company__description--hidden'));
+      }
+      if (mobileDescriptions) {
+        mobileDescriptions.forEach((elem) => elem.classList.remove('about-company__mobile-description--hidden'));
+      }
+      if (button) {
+        button.classList.add('about-company__button--rollup');
+        button.textContent = 'свернуть';
+      }
     }
   });
 };
