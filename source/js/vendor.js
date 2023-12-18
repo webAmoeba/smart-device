6450a81ab4374dc7c516440bce2228c6c38b700c
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2023-12-18 20:43:56 +0400

    Update 2023-12-18T16:43:01.524Z

--- source/js/vendor.js
+++
@@ -1,9 +0,0 @@
-// Swiper 7.4.1
-// import './vendor/swiper';
-import './vendor/focus-visible-polyfill';
-
-window.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('load', () => {
-    // initPhoneMask();
-  });
-});
